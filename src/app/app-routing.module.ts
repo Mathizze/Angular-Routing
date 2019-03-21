714a841c017062bef425533d40f4ebcaee41c804
mathijs.hardeman <mathijs.hardeman@MC-C2VVBG2>
2019-03-21 15:33:42 +0100

    routing to features

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { WelcomeComponent } from './home/welcome.component';
+import { PageNotFoundComponent } from './page-not-found.component';
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot([ 
+            { path: 'welcome', component: WelcomeComponent },
+            { path: '', pathMatch: 'full', redirectTo: 'welcome' },
+            { path: '**', component: PageNotFoundComponent }
+        ])
+    ],
+    exports: [RouterModule],
+    declarations: [],
+    providers: [],
+})
+export class AppRoutingModule { }
