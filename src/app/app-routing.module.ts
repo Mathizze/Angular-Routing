5eb584ca339cf130decec0fd12971ec024dd4a34
mathijs.hardeman <mathijs.hardeman@MC-C2VVBG2>
2019-04-01 11:21:25 +0200

    Lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
+import { AuthGuard } from './user/auth.guard';
+import { SelectiveStrategy } from './selective-strategy.service';
 
 @NgModule({
     imports: [
         RouterModule.forRoot([ 
             { path: 'welcome', component: WelcomeComponent },
+            { 
+                path: 'products', 
+                canActivate: [AuthGuard],
+                data: { preload: true },
+                loadChildren: './products/product.module#ProductModule'},
             { path: '', pathMatch: 'full', redirectTo: 'welcome' },
             { path: '**', component: PageNotFoundComponent }
-        ])
+        ], { preloadingStrategy: SelectiveStrategy })
     ],
     exports: [RouterModule],
     declarations: [],
